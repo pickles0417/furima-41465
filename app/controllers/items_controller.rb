c531f472bb9bea52a83d56d633f27f3f84732688
pickles0417 <tkm2933@icloud.com>
2024-09-01 18:39:47 +0900

    itemsテーブルとモデル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.includes(:user)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
   end
 end
