bd4e814ffb51ff98ff1c368003344b7325c43857
pickles0417 <tkm2933@icloud.com>
2024-09-01 19:22:10 +0900

    activestorege準備

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,5 +9,15 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+  end
+  
+  private
+
+  def item_patams
+    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 end
