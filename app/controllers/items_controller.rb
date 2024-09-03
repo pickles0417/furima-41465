80a3253ef95355018c81a7497054aa7074f20d70
pickles0417 <tkm2933@icloud.com>
2024-09-03 18:21:40 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :authorize_user!, only: [:edit, :update]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
   def index
     @items = Item.all.order("created_at DESC")
   end
