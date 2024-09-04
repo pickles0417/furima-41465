a62f5512a975f895445cc00f6652224709e83bd2
pickles0417 <tkm2933@icloud.com>
2024-09-05 06:11:03 +0900

    ordersコントローラー編集

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,30 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :non_purchased_item, only: [:index, :create]
+
+  def index
+    @order_form = OrderForm.new
+  end
+
+  def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      @order_form.save
+      redirect_to root_path
+    else
+      render 'index', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def non_purchased_item
+    # itemがあっての、order_form（入れ子構造）。他のコントローラーで生成されたitemを使うにはcreateアクションに定義する。
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+  end
+end
