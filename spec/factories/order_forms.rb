70456df1bcaf087fb1efe0fd68edcb7df16ec92e
pickles0417 <tkm2933@icloud.com>
2024-09-05 15:51:51 +0900

    単体テストコード作成と編集

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :order_form do
-    
+    user_id { Faker::Number.non_zero_digit }
+    item_id { Faker::Number.non_zero_digit }
+    postcode { '123-4567' }
+    prefecture_id { 1 }
+    city { '東京都' }
+    block { '1-1' }
+    building { '東京ハイツ' }
+    phone_number { '09000980987' }
   end
 end
