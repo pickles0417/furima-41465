13f0e5b1d2407cecdc0e41556990a2ad652bc384
pickles0417 <tkm2933@icloud.com>
2024-09-05 15:53:19 +0900

    ordersコントローラー編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,8 +22,8 @@ class OrdersController < ApplicationController
     params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
+
   def non_purchased_item
-    # itemがあっての、order_form（入れ子構造）。他のコントローラーで生成されたitemを使うにはcreateアクションに定義する。
     @item = Item.find(params[:item_id])
     redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
