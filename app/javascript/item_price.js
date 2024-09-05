71f205af9a20641106f92b68e302fb5d915bbf77
pickles0417 <tkm2933@icloud.com>
2024-09-06 00:52:13 +0900

    item.js編集

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,5 +1,6 @@
 const price = () => {
   const priceInput = document.getElementById("item-price");
+  if (!priceInput){ return false;}
   priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
   const addTaxDom = document.getElementById("add-tax-price");
