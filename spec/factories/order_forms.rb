f4c7537f9d0e725acdc02c333168e79bea8b4dab
pickles0417 <tkm2933@icloud.com>
2024-09-06 17:33:03 +0900

    修正２

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :order_form do
-    user_id { association :user }
-    item_id { association :item }
     postcode { '123-4567' }
     prefecture_id { 1 }
     city { '東京都' }
